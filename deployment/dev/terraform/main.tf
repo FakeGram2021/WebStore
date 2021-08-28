a4ddf7e8e8e4c8f8c06dc77f04457772a4920747
Mihailo Djokic <djokic.mihailo@gmail.com>
2021-08-28 22:24:55 +0200

    Ordering deployment scripts

--- deployment/dev/terraform/main.tf
+++ deployment/dev/terraform/main.tf
@@ -6,6 +6,12 @@ terraform {
 provider "heroku" {
 }
 
+resource "heroku_addon" "database" {
+  app  = heroku_app.inventory-management.name
+  plan = "heroku-postgresql:hobby-dev"
+
+}
+
 resource "heroku_app" "inventory-management" {
   name   = "dev-agent-inventory-management"
   region = "eu"
@@ -23,19 +29,36 @@ resource "heroku_build" "inventory-management" {
   }
 }
 
-resource "heroku_addon" "database" {
-  app  = heroku_app.inventory-management.name
-  plan = "heroku-postgresql:hobby-dev"
+resource "heroku_app" "ordering" {
+  name   = "dev-agent-ordering"
+  region = "eu"
+  stack  = "container"
+  config_vars = {
+    JWT_SECRET = var.JWT_SECRET
+  }
+}
 
+resource "heroku_build" "ordering" {
+  app = heroku_app.ordering.id
+  depends_on = [heroku_build.inventory-management]
+  source {
+    path = "ordering"
+  }
+}
+
+resource "heroku_addon_attachment" "ordering_database" {
+  app_id = heroku_app.ordering.id
+  addon_id = heroku_addon.database.id
 }
 
 resource "heroku_app" "frontend" {
   name   = "dev-agent-frontend"
   region = "eu"
   stack  = "container"
-  depends_on = [heroku_build.inventory-management]
+  depends_on = [heroku_build.ordering]
   config_vars = {
     INVENTORY_MANAGEMENT_API = "${heroku_app.inventory-management.web_url}api"
+    ORDERING_API = "${heroku_app.ordering.web_url}api"
   }
 }
 
@@ -50,6 +73,11 @@ resource "heroku_build" "frontend" {
 output "inventory-management_app_url" {
   value = "https://${heroku_app.inventory-management.name}.herokuapp.com"
 }
+
+output "ordering_app_url" {
+  value = "https://${heroku_app.ordering.name}.herokuapp.com"
+}
+
 output "frontend_app_url" {
   value = "https://${heroku_app.frontend.name}.herokuapp.com"
 }
