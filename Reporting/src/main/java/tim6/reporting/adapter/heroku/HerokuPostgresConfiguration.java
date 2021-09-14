9d0eda441d51544c821ddf9bcd9337f0581299d8
Mihailo Djokic <djokic.mihailo@gmail.com>
2021-09-14 19:58:08 +0200

    Added missing heroku postgres configuration to reporting service

---
+++ Reporting/src/main/java/tim6/reporting/adapter/heroku/HerokuPostgresConfiguration.java
@@ -0,0 +1,36 @@
+package tim6.reporting.adapter.heroku;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(
+        value = "deployment",
+        havingValue = "heroku"
+)
+public class HerokuPostgresConfiguration {
+
+    @Value("${DATABASE_URL}")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
